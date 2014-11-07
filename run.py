6ddc5892f03c6e341cf890fb4f728fec794c053b
shane.eller@gmail.com <shane.eller@gmail.com>
2014-11-07 19:08:59 +0000

    making magic happen

--- run.py
+++ run.py
@@ -1,8 +1,19 @@
 from eve import Eve
+from os import walk, path
+
 #from tendo import singleton
 #me = singleton.SingleInstance()
 
 app = Eve()
 
+extra_dirs = ['./',]
+extra_files = extra_dirs[:]
+for extra_dir in extra_dirs:
+    for dirname, dirs, files in walk(extra_dir):
+        for filename in files:
+            filename = path.join(dirname, filename)
+            if path.isfile(filename):
+                extra_files.append(filename)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,extra_files=extra_files)
